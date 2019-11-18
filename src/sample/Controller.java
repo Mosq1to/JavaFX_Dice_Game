3e7118df00f3c0920c5f3a1d5cd95221f1ce1cd9
Mosq1to <Manjit_89@hotmail.com>
2019-11-19 00:10:54 +0100

    front base done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,42 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
 public class Controller {
-}
+
+    @FXML
+    private Button exitBtn;
+    @FXML
+    private Button startBtn;
+    @FXML
+    private ComboBox players;
+
+
+    @FXML
+    void exit(ActionEvent event) {
+        System.exit(0);
+    }
+
+
+    @FXML
+    void startgame(ActionEvent event) {
+        String spelare = players.getValue().toString();
+        System.out.println(spelare);
+
+
+    }
+
+
+
+    @FXML
+    void initialize() {
+        players.getItems().addAll("2","3","4","5");
+
+    }
+
+
+
+    }
